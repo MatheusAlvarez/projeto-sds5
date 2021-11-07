abf6ef674badb5c40526f65d4af7ba63c02b0f5e
Matheus Maia Alvarez <mthalvarez0000@hotmail.com>
2021-11-06 23:17:39 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,27 @@
-export type SaleSum =  {
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
+export type SaleSum = {
     sellerName: string;
     sum: number;
 }
