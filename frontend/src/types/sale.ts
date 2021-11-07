f7fa6378ef9b60eeda01def101b0d89a5de69237
Matheus Maia Alvarez <mthalvarez0000@hotmail.com>
2021-11-06 22:36:14 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,11 @@
 export type SaleSum =  {
-    sellerName: string,
-    sum: number
+    sellerName: string;
+    sum: number;
 }
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
+}
+
